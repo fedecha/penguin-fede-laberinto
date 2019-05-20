add7a13a9248c666ee5c87d0cb2ca9ac21489819
nandyme <38559436+nandyme@users.noreply.github.com>
2019-05-20 09:37:20 -0400

    Agrego unas cositas xD

--- adivinanza.py
+++ adivinanza.py
@@ -1,24 +1,20 @@
-#Juego de adivinar el numero, todos juntos,
-#adivinar un numero generado aleatoriamente,
-#ir introduciendo por teclado el dato a adivinar
-
 from random import randint
 generado=randint(0,10) #generamos el numero aleatorio
 print(generado) #trampa
-condicion=True  
-intento=0
+#condicion=True  
+intento=3
 
-while condicion:
-    if intento>0:
-        numero=input("dame nro de 0-10")
-        intento=intento-1 #intento+=1
+print('tenés ',intento, ' intentos' )
+while intento>0:
+        numero=input("dame nro de 0-10: ")
+        #intento-=1 #intento+=1 #SE MOVIÓ A LA LINEA 16
         if generado==int(numero): #comparamos el numero introducido
-        print("ganaste algo")
-        condicion=False
+            print("ganaste algo")
+            break #ya que el jugador ganó, se detiene el bucle con "break"
         else:
-        print("no ganaste nada")
-        print("te quedan",intento,"intentos")
-    else:
-        condicion=False
-        print(perdiste)
-    
+            print("fallaste")
+            intento-=1 #intento+=1  #se cambia de lugar, ya que si se acierta en el primer intento
+                                    #no es necesario ejecutar esta linea
+            print("te quedan ",intento," intentos")
+            if intento == 0:
+                print('perdiste')
